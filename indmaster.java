a646ed50259fb534a82d02517f83be542fab6dd9
Joonsik Kim <kimkimjunior2001@gmail.com>
2022-09-15 01:25:10 -0700

    Create indmaster.java
    Added indmaster.java

---
+++ indmaster.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class individual {
+	
+	public static void main(String[] args) {
+		int rand = (int) (Math.random()*100);
+		System.out.println("Your number is " + rand);
+	}
+
+}

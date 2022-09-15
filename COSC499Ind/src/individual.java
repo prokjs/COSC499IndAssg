d41dae23a5c37d2021fe8af3460813452d541017
Joonsik Kim <kimkimjunior2001@gmail.com>
2022-09-15 01:59:05 -0700

    Same changes for the left side
    Junits and fixed readability

--- COSC499Ind/src/individual.java
+++ COSC499Ind/src/individual.java
@@ -6,15 +6,18 @@ public class individual {
 		Scanner scan = new Scanner(System.in); //Scanner scans user input
 		int rand = (int) (Math.random()*100); //Random number
 		System.out.println("Your number is " + rand);
-		System.out.println("Choose left /");
-		String temp = scan.nextLine(); 
-		if(temp.equals("left")) { //This if statement checks if the left has been input by the user, to check to add 1 or say invalid
-			rand--;
-			System.out.println(rand);
-		}else {
-			System.out.println("Invalid Input");
-		}
+		System.out.println("Choose left");
+		String temp = scan.nextLine();
+		rand += left(temp);
+		System.out.println(rand);
 		scan.close();
 	}
-
+	
+	public static int left(String temp) {
+		if(temp.equals("left")){//This if statement checks if the left has been input by the user, to check to add 1
+			return 1;
+		}else {
+			return 0;
+		}
+	}
 }

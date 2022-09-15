d8cb8d5e492a761ebaba50044d2d4690156c8759
Joonsik Kim <kimkimjunior2001@gmail.com>
2022-09-15 01:37:40 -0700

    Added inline description
    Explains what the code does.

--- COSC499Ind/src/individual.java
+++ COSC499Ind/src/individual.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class individual {
 	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int rand = (int) (Math.random()*100);
+		Scanner scan = new Scanner(System.in); //Scanner scans user input
+		int rand = (int) (Math.random()*100); //Random number
 		System.out.println("Your number is " + rand);
 		System.out.println("Choose left /");
-		String temp = scan.nextLine();
-		if(temp.equals("left")) {
+		String temp = scan.nextLine(); 
+		if(temp.equals("left")) { //This if statement checks if the left has been input by the user, to check to add 1 or say invalid
 			rand--;
 			System.out.println(rand);
 		}else {
